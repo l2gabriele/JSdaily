05fd404e5fb0b40a01c40d0a65507894f918e5a9
ed42311 <ed42311@gmail.com>
2017-05-03 17:34:29 -0600

    removed the answer

--- setsOfThree/set2/zeroString.js
+++ setsOfThree/set2/zeroString.js
@@ -1,6 +1,6 @@
 var exports = module.exports = {};
 
-exports.aStringPassedToThis = (willReturnWithZerosInstead) => {
-  var replaced = willReturnWithZerosInstead.replace(/ /g, 0);
-  return replaced;
+exports.aStringPassedToThis = (willReturnWithZerosInsteadOfSpaces) => {
+   // The variable passed to this must be a string
+   // When the string is returned it must have 0 instead ' '
 }
