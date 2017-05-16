b8553fb5f7eb4b530ad1094a623eb42d2536ce5c
ed42311 <ed42311@gmail.com>
2017-05-16 13:15:38 -0600

    need to switch over

--- setsOfThree/set5/set5.js
+++ setsOfThree/set5/set5.js
@@ -2,8 +2,20 @@ import request from 'ajax-request';
 import deck from '../../resources/deck.json';
 import cities from '../../resources/cities.json';
 
+/***********************
+  Exercise 1: This function takes 4 arguments: age, average age at death, amount per day, type of snack
+    calculates the amount consumed for rest of the life
+    you returned value should be an object that holds years of life left as "years", amount of total snacks consumed as "total",
+    and a user output as "readable" that equals:
+       "You will need N of T to last you until the ripe old age of X"
+***********************/
+
+function lifetimeSupply(){
+
+}
+
 /************************
-  Exercise 1: This function will create a simple calculator, it should be able
+  Exercise 2: This function will create a simple calculator, it should be able
   to add subtract and divide.  The returned object of the calculator should have
   a human readable output field called "readable" and a output of the calculation
   called "output".  If either of the two number parameters are not numbers the object
@@ -11,7 +23,7 @@ import cities from '../../resources/cities.json';
   "number 1 is not a number"
 ************************/
 
-function createCalc(calc, num1, num2){
+function simpleCalc(calc, num1, num2){
   let operation = '';
   let obj = {};
 
@@ -43,18 +55,6 @@ function createCalc(calc, num1, num2){
 };
 
 /***********************
-  Exercise 2: This function takes 4 arguments: age, average age at death, amount per day, type of snack
-    calculates the amount consumed for rest of the life
-    you returned value should be an object that holds years of life left as "years", amount of total snacks consumed as "total",
-    and a user output as "readable" that equals:
-       "You will need N of T to last you until the ripe old age of X"
-***********************/
-
-function lifetimeSupply(){
-
-}
-
-/***********************
   Exercise 3: Write a function that accepts a string, and returns true if the string is a palindrome
 ***********************/
 
@@ -67,7 +67,5 @@ function isPalindrome(){
 ************************/
 
 module.exports = {
-     lifetimeSupply: lifetimeSupply,
-     pupCalc: pupCalc,
-     isPalindrome: isPalindrome
+     simpleCalc: simpleCalc
 }
--- setsOfThree/set5/test.js
+++ setsOfThree/set5/test.js
@@ -2,12 +2,47 @@ const set5 = require('./set5');
 import chai from 'chai';
 const expect = chai.expect;
 
-describe('group of tests #1', function () {
-  it('test description #2 (e.g. "should add numbers together")', function () {
-    expect(thing1).to.equal(thing2); // does thing1 === thing2?
-    expect(...).to.eql(...); // does thing1 deepEqual thing2? (good for arrays, objects)
+describe('Set #4', function(){
+
+  describe('Exercise 1: pupCalc', function(){
+
+    it('should return a number', function(){
+      expect(set4.pupCalc(4)).to.be.a('number');
+    });
+
+    it('should return the age of your puppy in dog years', function(){
+      expect(set4.pupCalc(4)).to.eql(28);
+    });
+
   });
-  it('should do this other thing', function () {
-    expect(thing).to.be.a.('function'); // is typeof thing a <blank>?
+
+  describe('Exercise 2: Lifetime Supply', function() {
+
+    const answer = set4.lifetimeSupply(29, 90, 1, "candy BBAaaaaRRss");
+    it('should return an object', function(){
+      expect(answer).to.be.an('object');
+    });
+
+    it('the returned object should have the keys year, total, and readable', function(){
+      expect(answer).to.have.all.keys('years', 'total', 'readable');
+    });
+
+    it('the year field should be equal to the total number of years yet to be lived', function(){
+      expect(answer.years).to.eql(55);
+    });
+
+    it('the total field should equal the total number of snacks to be consumed', function(){
+      expect(answer.total).to.eql(20075);
+    });
+
+    it('the total field should equal the total number of snacks to be consumed', function(){
+      expect(answer.readable).to.be.an('string');
+    });
+  });
+
+  describe('Exercise 3: Palindromes', function(){
+    it('This function accepts a string, and must return true or false if the string is a palindrome', function(){
+      expect(set4.isPalindrome("racecar")).to.eql(true);
+    });
   });
 });
