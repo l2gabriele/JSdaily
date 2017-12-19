3f1e6fea27968872bdfde7cd6f7fbcdb7763537e
ed42311 <ed42311@gmail.com>
2017-12-18 18:09:09 -0700

    modified the es6 stuff, need set 9

--- setsOfThree/set1/set1.js
+++ setsOfThree/set1/set1.js
@@ -1,23 +1,50 @@
 import cities from '../../resources/cities.json';
 const exports = module.exports = {};
 
-
-// TODO:  check rewrites on mocha tests
-
+/************************
+  Exercise 1: Get the year you were born
+  This function should take in your age and return
+  The year you are born
+************************/
 
 exports.returnWhatYearYouWereBorn = function(age){
-  //  This function should take in your age and return
-  //  The year you are born
+    let date = new Date();
+    return date.getFullYear() - age;
 };
+
+
+/************************
+  Exercise 2:
+  You should sort through the cities data set and return all the cities that begin with the letter c
+  this operation must be case insensitive
+************************/
 
 exports.returnOnlyCitiesThatStartWith = function(theLetter){
-  //  You should sort through the cities data set and return all the cities that begin with the letter c
-  //  this operation must be case insensitive
-  return theLetter
+  console.log('start');
+
+  // return cities.filter((city, index, array) => {
+  //   if(city.city.slice(0,1).toLowerCase() === theLetter.toLowerCase()) {
+  //     return city;
+  //   }
+  // })
+
+  var retArr = [];
+  for(var index = 0; index < cities.length; index++) {
+    if(cities[index].city.slice(0,1).toLowerCase() === theLetter.toLowerCase()) {
+      retArr.push(cities[index]);
+    };
+  }
+  return retArr;
 };
 
+exports.returnOnlyCitiesThatStartWith("c");
+
+/************************
+  Exercise 3:
+  You need to find all the prime numbers between 2 and 2,000,000 then sum then together
+  and return them...
+************************/
+
 exports.returnSumOfPrimes = function(input) {
-    //  You need to find all the prime numbers between 2 and 2,000,000 then sum then together
-    //  and return them...
     return input;
 };
---
+++ setsOfThree/set9/set9.js
@@ -0,0 +1,44 @@
+import fetch from 'node-fetch';
+import deck from '../../resources/deck.json';
+
+/************************
+  Exercise 1:
+************************/
+
+function aintNobody(){
+
+}
+
+// aintNobody();
+
+/***********************
+  Exercise 2:
+
+***********************/
+
+function whatsUrBestDay(){
+
+}
+
+// whatsUrBestDay();
+
+/***********************
+  Exercise 3:
+***********************/
+
+function tresRando(){
+
+};
+
+// uncomment to test
+// tresRando();
+
+/************************
+  Ignore this below!
+************************/
+
+module.exports = {
+     aintNobody: aintNobody,
+     whatsUrBestDay: whatsUrBestDay,
+     tresRando: tresRando
+}
